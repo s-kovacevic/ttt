b278eae0f4b5fab1d945c07a370071f5d3cf2058
Savo Kovacevic <savo.s.kovacevic@gmail.com>
2017-12-01 17:45:06 +0100

    Added static files and base and index templates, setup index handler

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import logging
-from flask import Flask
+from flask import Flask, render_template
 from util import setup_logger, get_config
 
 setup_logger()
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     import argparse
